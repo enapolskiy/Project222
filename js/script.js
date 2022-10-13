6abbd4be02fcd38a76dc5aceba675f14c6cba06b
enapolskiy83@yandex.ru <enapolskiy@gmail.com>
2022-10-13 16:45:20 +0700

    qwe

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 // 'use strict';
 
-alert ('hello');
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt("Один из последних фильмов что вы посмотрели", ""),
+      b = prompt("На сколько вы его оцениваете", ""),
+      c = prompt("Один из последних фильмов что вы посмотрели", ""),
+      d = prompt("На сколько вы его оцениваете", "");
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
